69964730d27d668f63cbe84839c94287b2c56631
allen <xuyayang2012@163.com>
2024-02-03 14:34:17 +0800

    f

--- set.go
+++ set.go
@@ -24,6 +24,24 @@ func DiffSet(a []int64, b []int64) []int64 {
 	return diff
 }
 
+func DiffSetStr(a []string, b []string) []string {
+	var diff []string
+	mp := make(map[string]int)
+	for _, n := range b {
+		if _, ok := mp[n]; !ok {
+			mp[n] = 1
+		}
+	}
+
+	for _, n := range a {
+		if _, ok := mp[n]; !ok {
+			diff = append(diff, n)
+		}
+	}
+
+	return diff
+}
+
 func InArray(needle interface{}, haystack interface{}) bool {
 	val := reflect.ValueOf(haystack)
 	switch val.Kind() {
